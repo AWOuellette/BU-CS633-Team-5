1d52c15fedaab7950befe0109dc3d09435d3bfa7
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-01-27 02:51:08 -0500

    Initial commit

---
+++ BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/BuCourseInquiryApplication.java
@@ -0,0 +1,13 @@
+package BU.MET.CS3.Team5.BU.Course.Inquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuCourseInquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuCourseInquiryApplication.class, args);
+	}
+
+}
---
+++ BU-Course-Inquiry/src/test/java/BU/MET/CS3/Team5/BU/Course/Inquiry/BuCourseInquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package BU.MET.CS3.Team5.BU.Course.Inquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuCourseInquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
