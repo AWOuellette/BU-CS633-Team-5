48b9db89f3586c8bf0e3df3c11ff8cd1f7285efe
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-01-27 15:11:46 -0500

    Add additional API endpoint to get course by ObjectId

--- BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/CourseController.java
+++ BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/CourseController.java
@@ -2,14 +2,17 @@ package BU.MET.CS3.Team5.BU.Course.Inquiry.controller;
 
 import BU.MET.CS3.Team5.BU.Course.Inquiry.model.Course;
 import BU.MET.CS3.Team5.BU.Course.Inquiry.service.CourseService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/courses")
@@ -19,5 +22,9 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<List<Course>> getAllCourses(){
         return new ResponseEntity<List<Course>>(courseService.allCourses(),HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Course>> getSingleCourse(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Course>>(courseService.singleCourse(id),HttpStatus.OK);
     }
 }
--- BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/service/CourseService.java
+++ BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/service/CourseService.java
@@ -2,16 +2,25 @@ package BU.MET.CS3.Team5.BU.Course.Inquiry.service;
 
 import BU.MET.CS3.Team5.BU.Course.Inquiry.model.Course;
 import BU.MET.CS3.Team5.BU.Course.Inquiry.repository.CourseRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
+
     @Autowired
     private CourseRepository courseRepository;
+
     public List<Course> allCourses(){
         return courseRepository.findAll();
     }
+
+    public Optional<Course> singleCourse(ObjectId id){
+        return courseRepository.findById(id);
+    }
+
 }
