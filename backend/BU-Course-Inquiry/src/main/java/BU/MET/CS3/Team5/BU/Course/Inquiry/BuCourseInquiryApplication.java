5a0a519853ffd802caf7175bd517dbd5aad0be17
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-02-17 18:15:05 -0500

    Fixed API Key delete bug

--- backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/BuCourseInquiryApplication.java
+++ backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/BuCourseInquiryApplication.java
@@ -2,8 +2,10 @@ package BU.MET.CS3.Team5.BU.Course.Inquiry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class BuCourseInquiryApplication {
 
 	public static void main(String[] args) {
