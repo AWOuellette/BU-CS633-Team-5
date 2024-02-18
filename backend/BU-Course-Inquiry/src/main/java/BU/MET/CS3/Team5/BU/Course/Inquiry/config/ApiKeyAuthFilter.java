fa5ae147ed2072b1ff84ebb0da8fed90e5aa8575
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-02-18 18:00:29 -0500

    Clean up code

--- backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/config/ApiKeyAuthFilter.java
+++ backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/config/ApiKeyAuthFilter.java
@@ -2,20 +2,14 @@ package BU.MET.CS3.Team5.BU.Course.Inquiry.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 @Component
 @RequiredArgsConstructor
