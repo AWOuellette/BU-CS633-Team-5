fe5ec36619a679ac90ceed8ea6edab937a853bfc
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-02-24 14:13:49 -0500

    Revert cross origins change

--- backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/ReviewController.java
+++ backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/ReviewController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
--- backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/TipController.java
+++ backend/BU-Course-Inquiry/src/main/java/BU/MET/CS3/Team5/BU/Course/Inquiry/controller/TipController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/tips")
 public class TipController {
     @Autowired
